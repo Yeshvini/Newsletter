2270afdfddc54836c539447f774492ef9c64eb8f
Yeshvini Anand <40928263+Yeshvini@users.noreply.github.com>
2020-05-01 19:49:59 +0530

    Update object_detection.py

--- object_detection.py
+++ object_detection.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 """
-
 @author: yeshvini
 """
 
@@ -16,7 +15,16 @@ im = cv2.imread('img.jpg')
 # returns bounding box with confidence scores for detected image
 bbox, label, conf = cv.detect_common_objects(im)
 
+# draw bounding box over detected objects
 output_image = draw_bbox(im, bbox, label, conf)
 
-plt.imshow(output_image)
-plt.show()
+# display output
+# press any key to close window  
+cv2.imshow("object_detection", output_image)
+cv2.waitKey()
+
+# save output
+cv2.imwrite("object_detection.jpg", output_image)
+
+# release resources
+cv2.destroyAllWindows()
