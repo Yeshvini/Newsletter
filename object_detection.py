a10fcaabe67e2649ffc61ad9cdc67e1958d561b6
Yeshvini Anand <40928263+Yeshvini@users.noreply.github.com>
2020-05-01 19:18:04 +0530

    Add files via upload

---
+++ object_detection.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+
+@author: yeshvini
+"""
+
+#import necessary packages
+import cv2
+import matplotlib.pyplot as plt
+import cvlib as cv
+from cvlib.object_detection import draw_bbox
+
+# read image
+im = cv2.imread('img.jpg')
+
+# returns bounding box with confidence scores for detected image
+bbox, label, conf = cv.detect_common_objects(im)
+
+output_image = draw_bbox(im, bbox, label, conf)
+
+plt.imshow(output_image)
+plt.show()
